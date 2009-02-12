e326a244f994f840892caa61bb09c6b8336668c2
U-HM-LAPTOP\hmagnusson <hmagnusson@hm-laptop.(none)>
2009-02-12 18:46:02 +0900

    Added rotation animation and snap-to-grid (which needs work)

--- client.py
+++ client.py
@@ -6,12 +6,21 @@ import rabbyt
 class Piece(rabbyt.Sprite):
     def __init__(self, **kargs):
         rabbyt.Sprite.__init__(self, **kargs)
+        self.animflag = False
+    def animendcallback(self):
+        self.animflag = False
+    def width(self):
+        return self.right - self.left
+    def height(self):
+        return self.bottom - self.top
 
-size = (800, 600)
+size = (800.0, 600)
+ratio = (size[0]/size[1])
 
+print ratio
 pygame.init()
 pygame.display.set_mode(size, pygame.OPENGL | pygame.DOUBLEBUF)
-rabbyt.set_viewport(size, projection=(0, 0, 800, 600))
+rabbyt.set_viewport(size, projection=(0, 0, size[0], size[1]))
 rabbyt.set_default_attribs()
 
 pieces = [Piece(xy=attributes[0], texture=attributes[1]) for attributes in
@@ -37,7 +46,10 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_z:
         	    z += 10
-        	    rabbyt.set_viewport(size, projection=(z,z,800-z,600-z))
+        	    rabbyt.set_viewport(size, projection=(ratio*z,z,size[0]-ratio*z,size[1]-z))
+            if event.key == pygame.K_x:
+        	    z -= 10
+        	    rabbyt.set_viewport(size, projection=(ratio*z,z,size[0]-ratio*z,size[1]-z))
             if event.key in (pygame.K_ESCAPE, pygame.K_q):
                 running=False
         elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -53,11 +65,15 @@ while running:
                 pieces.append(grabbed_point)
                 grabbed_point.rgb = (.5, .5, .5)
                 #grabbed_point.scale = rabbyt.lerp(1, 1.25, dt=100, extend="reverse")
-            
-            if event.button == 4 and grabbed_point:
-                grabbed_point.rot += 15
-            elif event.button == 5 and grabbed_point:
-                grabbed_point.rot -= 15
+
+            if event.button == 4 and grabbed_point and grabbed_point.animflag == False:
+                grabbed_point.animflag = True
+                grabbed_point.rot = rabbyt.lerp(grabbed_point.rot, grabbed_point.rot + 45, dt=200);
+                rabbyt.scheduler.add(rabbyt.get_time()+200, grabbed_point.animendcallback)
+            elif event.button == 5 and grabbed_point and grabbed_point.animflag == False:
+                grabbed_point.animflag = True
+                grabbed_point.rot = rabbyt.lerp(grabbed_point.rot, grabbed_point.rot - 45, dt=200);
+                rabbyt.scheduler.add(rabbyt.get_time()+200, grabbed_point.animendcallback)
             else:
                 if grabbed_point:
                     offset_x = grabbed_point.x-event.pos[0]
@@ -71,10 +87,15 @@ while running:
         elif event.type == pygame.MOUSEBUTTONUP:
             if grabbed_point:
                 grabbed_point.rgb = (1, 1, 1)
-                # grabbed_point.scale = rabbyt.lerp(1.25, 1, dt=200)
+                grabbed_point.xy = rabbyt.lerp((grabbed_point.x, grabbed_point.y), (grabbed_point.x - grabbed_point.x % grabbed_point.width(), grabbed_point.y - grabbed_point.y % grabbed_point.height()), dt=200)
+                #grabbed_point.scale = rabbyt.lerp(1.25, 1, dt=200)
             grabbed_point = None
 
     rabbyt.set_time(pygame.time.get_ticks())
     rabbyt.clear()
     rabbyt.render_unsorted(pieces)
+    rabbyt.scheduler.pump()
     pygame.display.flip()
+
+def test():
+    print "hello? wtf"
